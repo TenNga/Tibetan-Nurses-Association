36aa3448dc9ae4886ea54573e8a1bcf9ec0e4797
tennga <tenngawang6080@gmail.com>
2020-02-25 21:34:59 -0500

    burger menu drop down done

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,17 +1,37 @@
 import React from 'react';
+import './css/Menu.css';
 
 const menu = () => {
+
+    const burongOnClickHandler=()=>{
+        //I can make this class component and use state to toggle clicks
+        if(!document.querySelector(".menu-ls").style.display)
+            document.querySelector(".menu-ls").style.display = "block";
+        else
+        document.querySelector(".menu-ls").style.display = "";
+    }
+
     return(
-        <ul>
-            {/* <li>Home</li>
+        <>
+        <div className="burger" onClick={burongOnClickHandler}>
+            <div></div>
+            <div></div>
+            <div></div> 
+        </div>
+        
+        <ul className="menu-ls">
+            <li>Home</li>
             <li>About Us</li>
             <li>Gallery</li>
             <li>Events</li>
             <li>Educational</li>
             <li>Member</li>
-            <li>Contact</li> */}
+            <li>Contact</li>
             <li>Menu</li>
         </ul>
+
+        </>
+        
     )
 }
 export default menu;
