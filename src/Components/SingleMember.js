b72c232cd028377b279e7fef56381fa16adc915b
tennga <tenngawang6080@gmail.com>
2020-02-28 21:30:56 -0500

    member updated with image and name

--- src/Components/SingleMember.js
+++ src/Components/SingleMember.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './css/SingleMember.css';
 
-const SingleMember = () => {
+const SingleMember = (props) => {
     return(
         <div className="single-member">
-            <img src="Kundhen.jpg" />
-            <h4>Kundhen</h4>
-            <h6>(Nursing Edu. Outreach Coordinator)</h6>
+            <img src={props.memberInfo.img} />
+            <h4>{props.memberInfo.name}</h4>
+            <h6>{props.memberInfo.position}</h6>
         </div>
     )
 }
--- src/Container/AboutUs.js
+++ src/Container/AboutUs.js
@@ -10,6 +10,23 @@ const AboutUs = () => {
         para: "Tibetan Nurses Association (TNA) is a non-profitable organization founded by nurses from Bellevue Hospital in New York USA on April 2nd, 2016. The organization's primary goal is to unite all Tibetan Nurses and Nursing Students as a professional body under one umbrella at a national level. The purpose of this organization is to help nurses who graduated from India, Nepal, and America to get the Credentials Evaluation Service, Licensing in different States and find jobs. TNA will also serve the Tibetan communities in the United States through different health-related awareness programs. TNA also prioritize to help not only Nurses in America but also the people in Tibet, Nepal, and India through different educational projects and health-related programs as well.",
         linkContent: "TNA BY LAWS",
         link: ""}
+
+    const memberInfo = [
+        {img: "Pema_Dorjee.jpg", name: "Pema Dorjee", position: "President"},
+        {img: "tenTsering.jpg", name: "Tenzin Tsering", position: "(Vice President)"},
+        {img: "Nyima_Tsamchoe.jpg", name: "Nyima Tsamchoe", position: "(Vice President)"},
+        {img: "Tenzin_Wangmo.jpg", name: "Tenzin Wangmo", position: "(General Scretary)"},
+        {img: "Ninila_cr5g2h.jpg", name: "Ninila Rotta", position: "(Gen. Pro. Coordinator)"},
+        {img: "Chemi_Dolkar_py3opn.jpg", name: "Chemi Dolkar", position: "(Gen. Pro. Coordinator"},
+        {img: "Chime_Aboomtsang.jpg", name: "Chime Aboomtsang", position: "(Social Media Coordinator)"},
+        {img: "Tenzin_Tsundue.jpg", name: "Tenzin Tsundue", position: "(Accountant)"},
+        {img: "Passang_Dolma.jpg", name: "Passang Dolma", position: "(Pub. Relation Officer)"},
+        {img: "Tenzin_Youtso.jpg", name: "Tenzin Youtso", position: "(Resourse Outreach Coordinator)"},
+        {img: "Kundhen.jpg", name: "Kundhen", position: "Nursing Edu. Outreach Coordinator"},]
+
+    const handleMemberInfo = () => {
+        return memberInfo.map(member => <SingleMember memberInfo={member} />)
+    }
     return(
         <div className="about-container">
             <div className="history">
@@ -19,10 +36,7 @@ const AboutUs = () => {
                 <h2 id="header">board members</h2>
                 <h4 id="sub-header">nynj2019</h4>
                 <div className="member-container">
-                    <SingleMember />
-                    <SingleMember />
-                    <SingleMember />
-                    <SingleMember />
+                    {handleMemberInfo()}
                 </div>
             </div>
         </div>
