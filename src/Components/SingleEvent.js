a7554b2fbc92f8b22f30d1a760eef3f838dd2500
tennga <tenngawang6080@gmail.com>
2020-03-08 21:23:09 -0400

    single event component

---
+++ src/Components/SingleEvent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+const SingleEvent = () => {
+    return(
+        <div className="single-event-container">
+            <img src="nurses.jpg" />
+            <h3>Event Header</h3>
+            <p>Month xxth - month xxth</p>
+            <p>Description asdfajsdflajsdlfaslkdfjlskdjflkasjfflkasdflkasdlkfnsaldkfnn f nmlkasndvoiejflk snflanfanv nzxs;ldkfn ao;knfl;zsndv;alnv ;lnsdfokans;</p>
+        </div>
+    )
+}
+
+export default SingleEvent;
--- src/Container/Event.js
+++ src/Container/Event.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import SingleEvent from '../Components/SingleEvent';
+import SingleMember from '../Components/SingleMember';
 
 const Event = () => {
     return(
-        <h1>Event Page</h1>
+        <div className="event-container">
+            <h1>2020 Event Page</h1>
+            <h4>Tibetan Nurses Assocation</h4>
+            <SingleEvent />
+            <SingleEvent />
+            <SingleEvent />
+            <SingleEvent />
+            <SingleEvent />
+
+        </div>
+        
     )
 }
 
