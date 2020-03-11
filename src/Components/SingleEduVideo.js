1efab67e7e21c8962d07d2c28102318815956a6d
tennga <tenngawang6080@gmail.com>
2020-03-11 17:11:47 -0400

    educational page done and populated with data

--- src/Components/SingleEduVideo.js
+++ src/Components/SingleEduVideo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalVideo from 'react-modal-video'
+import './css/SingleEduVideo.css';
 import '../../node_modules/react-modal-video/scss/modal-video.scss';
 
 class SingleEduVideo extends React.Component {
@@ -11,17 +12,20 @@ class SingleEduVideo extends React.Component {
     this.setState({isOpen: true})
   }
 
+  
+
   render () {
+      const {title, videoId } = this.props.data
     return (
-      <div>
+      <div className="single-video-container">
         <ModalVideo 
             channel='youtube' 
             isOpen={this.state.isOpen} 
-            videoId='01QIPeFt3PM' 
+            videoId={videoId} 
             onClose={() => this.setState({isOpen: false})} 
-            ratio={'16:9'}
         />
-        <button onClick={this.openModal}>Video Title</button>
+        <img id="thumb" src={'https://img.youtube.com/vi/'+ videoId +'/0.jpg'} onClick={this.openModal} />
+        <h4 id="title">{title}</h4>
       </div>
     )
   }
--- src/Container/EducationalContainer.js
+++ src/Container/EducationalContainer.js
@@ -1,15 +1,53 @@
 import React from 'react';
 import SingleEduVideo from '../Components/SingleEduVideo';
+import './css/EducationalContainer.css';
 
 const EducationalContainer = () => {
+    const videoData = [
+        {
+            title: "episode 3 manstruation",
+            videoId: "UaI9v0Gyf0w"
+         },
+         {
+            title: "episode 4 Mutrition and reading food labels",
+            videoId: "yGSLLJ-j-yo"
+         },
+         {
+            title: "episode 5 Gastritis",
+            videoId: "elRuL_iToog"
+         },
+         {
+            title: "episode 6 Pursuing Education in Nursing & Beyond ",
+            videoId: "YxEyoxZ4Gpw"
+         },
+         {
+            title: "episode 7 TNA Health Survey Report",
+            videoId: "erabaqOvuQE"
+         },
+         {
+            title: "episode 8 Lets Talk Mental Health",
+            videoId: "7bDM1RFMwXA"
+         },
+         {
+            title: "episode 9 Hepatitis B",
+            videoId: "nym-shCGKho"
+         },
+         {
+            title: "episode 10 importance of dental health",
+            videoId: "bLXEtgqstBA"
+         },
+         {
+            title: "episode 11 Women's Health (HPV & Cervical cancer)",
+            videoId: "01QIPeFt3PM"
+         },
+]
+
+    const renderVideo = () => {
+        return videoData.map(d => <SingleEduVideo data = {d} />)
+    }
     return(
         <div className="video-container">
-            <SingleEduVideo />
-            <SingleEduVideo />
-            <SingleEduVideo />
-            <SingleEduVideo />
-            <SingleEduVideo />
-            <SingleEduVideo />
+           {renderVideo()}
         </div>
        
     )
