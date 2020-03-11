4a9519b0bd08f6400953c9c79d502ebb607faf27
tennga <tenngawang6080@gmail.com>
2020-03-11 16:39:43 -0400

    added npm youtube modal component

--- src/Components/SingleEduVideo.js
+++ src/Components/SingleEduVideo.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import ModalVideo from 'react-modal-video'
+import '../../node_modules/react-modal-video/scss/modal-video.scss';
 
-const SingleEduVideo = () => {
-    return(
-        <div className="single-video">
-            <h3>Video Title</h3>
-            <h5>Description</h5>
-        </div>
+class SingleEduVideo extends React.Component {
+    
+  state = {
+      isOpen: false
+    }
+  openModal =() =>{
+    this.setState({isOpen: true})
+  }
+
+  render () {
+    return (
+      <div>
+        <ModalVideo 
+            channel='youtube' 
+            isOpen={this.state.isOpen} 
+            videoId='01QIPeFt3PM' 
+            onClose={() => this.setState({isOpen: false})} 
+            ratio={'16:9'}
+        />
+        <button onClick={this.openModal}>Video Title</button>
+      </div>
     )
+  }
 }
 
 export default SingleEduVideo;
