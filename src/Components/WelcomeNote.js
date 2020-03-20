91134c555e8f70c346fc411a4b508b0d2485c61d
tennga <tenngawang6080@gmail.com>
2020-03-19 22:04:29 -0400

    desktop welcome note style done

--- src/Components/WelcomeNote.js
+++ src/Components/WelcomeNote.js
@@ -6,9 +6,15 @@ import { Link } from 'react-router-dom';
 const WelcomeNote = (props) => {
     return(
         <div className="welcome-container">
-            <p className="welcome-header">{props.data.header}</p>
-            <p className="welcome-para">{props.data.para}</p>
-           <Link to={"/about"}> <button>Learn More</button> </Link>
+            <div className="sub-container">
+                <div className="text-container">
+                    <p className="welcome-header">{props.data.header}</p>
+                    <p className="welcome-para">{props.data.para}</p>
+                    <Link to={"/about"}> <button>Learn More</button> </Link>    
+                </div>
+                <img src="home_aboutTNA.jpg" />
+            </div>
+           
         </div>
     )
 }
