ea124f27ee77d027aed8eeb1bc65273d8ece3b5b
kevin <tenngawang6080@gmail.com>
2020-03-25 13:59:48 -0400

    added gsap js animation

--- src/Components/WelcomeNote.js
+++ src/Components/WelcomeNote.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './css/WelcomeNote.css';
+import gsap from "gsap";
 
 import { Link } from 'react-router-dom';
 
-const WelcomeNote = (props) => {
+class WelcomeNote extends React.Component{
 
-    return(
-        <div className="welcome-container">
-            <div className="sub-container">
-                <div className="text-container">
-                    <p className="welcome-header">{props.data.header}</p>
-                    <p className="welcome-para">{props.data.para}</p>
-                    {props.source === "about"? <a href="TNA_december_10th.docx" download><button>TNA BY LAWS</button></a> : <Link to={"/about"}> <button>Learn More</button> </Link>    }
-                    {/* <Link to={"/about"}> <button>Learn More</button> </Link>     */}
+    componentDidMount(){
+        gsap.from(".text-container", {opacity: 0,  duration: 1, y: -50})
+        gsap.from("#welcome-img", {opacity: 0, duration: 1, delay: .8, y: 30})
+    }
+
+    
+    render(){
+        return(
+            <div className="welcome-container">
+                <div className="sub-container">
+                    <div className="text-container">
+                        <p className="welcome-header">{this.props.data.header}</p>
+                        <p className="welcome-para">{this.props.data.para}</p>
+                        {this.props.source === "about"? <a href="TNA_december_10th.docx" download><button>TNA BY LAWS</button></a> : <Link to={"/about"}> <button>Learn More</button> </Link>    }
+                        {/* <Link to={"/about"}> <button>Learn More</button> </Link>     */}
+                    </div>
+                    {this.props.source === "about"? null : <img id="welcome-img" src="home_aboutTNA.jpg" /> }
+                    
                 </div>
-                {props.source === "about"? null : <img src="home_aboutTNA.jpg" /> }
-                
+            
             </div>
-           
-        </div>
-    )
+        )
+    }
 }
 
 export default WelcomeNote;
--- src/Container/Home.js
+++ src/Container/Home.js
@@ -4,19 +4,33 @@ import WelcomeNote from '../Components/WelcomeNote';
 import AudioPlayer from '../Components/AudioPlayer';
 import WhatWeDo from './WhatWeDo';
 
-const Home = () => {
-    const homeWelcomeData = {
-        header: "Welcome To Tibetan Nurses Assocation",
-        para: "Tibetan health care workers across New York and New Jersey including: Physicians, Nurse Practitioners, Registered Nurses and other healthcare members formally established Tibetan Nurses Association (TNA) on April 2nd, 2016, as a registered non-profit organization to serve the Tibetan Community.",
-        link: ""}
-    return(
-            <>
-                <CarouselSlider />
-                <WelcomeNote data={homeWelcomeData}/>
-                <AudioPlayer />
-                <WhatWeDo />
-            </>
-    )
+class Home extends React.Component {
+
+    
+    state = {
+            scrollY: ""
+        }
+
+        componentDidMount() {
+            window.addEventListener('scroll',()=> this.setState({scrollY: window.scrollY}))
+        }
+    render(){
+        console.log("Y Position: ", this.state.scrollY)
+        const homeWelcomeData = {
+                    header: "Welcome To Tibetan Nurses Assocation",
+                    para: "Tibetan health care workers across New York and New Jersey including: Physicians, Nurse Practitioners, Registered Nurses and other healthcare members formally established Tibetan Nurses Association (TNA) on April 2nd, 2016, as a registered non-profit organization to serve the Tibetan Community.",
+                    link: ""}
+        
+        return(
+                <>
+                    <CarouselSlider />
+                    <WelcomeNote data={homeWelcomeData}/>
+                    {this.state.scrollY >= 100? <AudioPlayer /> : null }
+                    {this.state.scrollY >= 1000? <WhatWeDo /> : null }
+                    
+                </>
+        )
+    }
 }
 
 export default Home;
