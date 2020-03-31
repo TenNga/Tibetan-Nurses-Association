fb6d66b6894285f1fdbffa6049bb131df676ce8f
kevin <tenngawang6080@gmail.com>
2020-03-31 13:26:02 -0400

    added React router to footer links

--- src/Components/FooterSection.js
+++ src/Components/FooterSection.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import uniqid from 'uniqid';
+import {Link } from 'react-router-dom';
 
 import './css/FooterSection.css';
 
 const FooterSection = (props) => {
+
+    const handleOnClick = () => {
+        window.scrollTo(0, 0);
+    }
     const handleSubHeader = () => {
-        return props.sectionData.subHeader.map(subHeader => <h4  key={uniqid()}>{subHeader}</h4>)
+        return props.sectionData.subHeader.map(subHeader => <Link to={"/"+subHeader.link} ><h4 onClick={handleOnClick} key={uniqid()}>{subHeader.text}</h4></Link>)
     }
     return(
         <div className="footer-section">
--- src/Container/Footer.js
+++ src/Container/Footer.js
@@ -6,9 +6,9 @@ import FooterSection from '../Components/FooterSection';
 
 const Footer = () => {
     const footerData = [
-        {header: "about us", subHeader: ["our mission", "members", "other branches"]},
-        {header: "Resources", subHeader: ["educational","opportunities"]},
-        {header: "contact us", subHeader: ["(347)-761-6304", "(718)-316-5571", "tibetannurses2016@gmail.com"]},
+        {header: "about us", subHeader: [{text: "our mission", link: "about"}, {text: "members", link: "member"}, {text: "other branches", link: "https://www.facebook.com/tna.mn.1"}]},
+        {header: "Resources", subHeader: [{text: "educational", link: "educational"},{text: "opportunities", link: ""}]},
+        {header: "contact us", subHeader: [{text: "(347)-761-6304", link: ""}, {text: "(718)-316-5571", link: ""}, {text: "tibetannurses2016@gmail.com", link: ""}]},
     ]
 
     const handleFooterSection = () => {
